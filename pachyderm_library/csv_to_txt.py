41465617597c5c9fd28cfe3cb037cc48bae9c93e
jonashaldemann <104436733+jonashaldemann@users.noreply.github.com>
2024-07-29 16:08:12 +0200

    pachyderm library corrections

--- pachyderm_library/csv_to_txt.py
+++ pachyderm_library/csv_to_txt.py
@@ -2,7 +2,7 @@ import csv
 
 rows = []
 
-with open("pachyderm_library\pachyderm_library.csv") as file:
+with open("jh_random\pachyderm_library\pachyderm_library.csv") as file:
     reader = csv.reader(file)
     for row in reader:
         text = row[0]
@@ -28,7 +28,7 @@ file.close()
 
 rows = rows[1:]
 
-textfile = open("pachyderm_library\Pach_Materials_Library.txt", "w")
+textfile = open("jh_random\pachyderm_library\Pach_Materials_Library.txt", "w")
 for row in rows:
     textfile.write(row)
     textfile.write("\n")
