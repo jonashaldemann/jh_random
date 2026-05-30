ede4a9fae3fa85512e5605cd0d9cb9b536401bc8
jonashaldemann <jonasha@users.noreply.github.com>
2026-05-30 14:33:49 +0200

    Add script to convert JPG and PNG images to GIF

---
+++ gif_from_jpgs/gif_from_jpgs.py
@@ -0,0 +1,23 @@
+import glob
+import os
+from PIL import Image
+
+ordner = "/Users/jonashaldemann/Downloads"
+
+bilder = sorted(glob.glob(os.path.join(ordner, "*.jpg")))
+bilder += sorted(glob.glob(os.path.join(ordner, "*.png")))
+
+frames = []
+for pfad in bilder:
+    img = Image.open(pfad).convert("RGB")
+    frames.append(img)
+
+output_path = os.path.join(ordner, "gif.gif")
+
+frames[0].save(
+    output_path,
+    save_all=True,
+    append_images=frames[1:],
+    duration=1000,
+    loop=0
+)
